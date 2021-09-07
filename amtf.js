9b1ff3bf9b38846ae2754cec67fddd6483ae5bec
Donghao Huang <donghao.huang@mastercard.com>
2021-09-08 06:01:49 +0800

    fixed lint

--- amtf.js
+++ amtf.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import dotEnv from 'dotenv-flow';
+import express from "express";
+import dotEnv from "dotenv-flow";
 import { installProxyMiddlewares } from "./lib/proxyMiddlewares.js";
 
 const NODE_ENV_DEV = "development";
@@ -7,7 +7,9 @@ const nodeEnv = process.env.NODE_ENV || NODE_ENV_DEV;
 console.log(`nodeEnv: ${nodeEnv}`);
 process.env.NODE_ENV = nodeEnv;
 dotEnv.config();
-console.log(`process.env.VUE_APP_PARSE_SERVER_URL: ${process.env.VUE_APP_PARSE_SERVER_URL}`);
+console.log(
+  `process.env.VUE_APP_PARSE_SERVER_URL: ${process.env.VUE_APP_PARSE_SERVER_URL}`
+);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,5 +17,5 @@ const port = process.env.PORT || 3000;
 installProxyMiddlewares(app);
 
 app.listen(port, function () {
-  console.log(`app is listening on port: ${port}`);
+  console.log(`${new Date()} - app is listening on port: ${port}`);
 });
