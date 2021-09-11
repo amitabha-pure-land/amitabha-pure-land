11307af68fb30541a4ce0811aec4de00e0953b33
inflaton <inflaton.sg@gmail.com>
2021-09-12 05:58:56 +0800

    enable cors

--- amtf.js
+++ amtf.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotEnv from "dotenv-flow";
 import { installProxyMiddlewares } from "amtf-proxy";
+import cors from "cors";
 
 const NODE_ENV_DEV = "development";
 const nodeEnv = process.env.NODE_ENV || NODE_ENV_DEV;
@@ -12,6 +13,8 @@ console.log(`SERVER_BASE_URL: ${process.env.SERVER_BASE_URL}`);
 console.log(`DYNAMIC_PAGE_HOST: ${process.env.DYNAMIC_PAGE_HOST}`);
 
 const app = express();
+app.use(cors());
+
 const port = process.env.PORT || 3000;
 
 installProxyMiddlewares(app);
