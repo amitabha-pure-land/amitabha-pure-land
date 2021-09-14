4f325f24239ac32f8b49b684d8f571d5e55bea8c
Inflaton <inflaton.sg@gmail.com>
2021-09-14 11:36:34 +0000

    added debug flag

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,12 +6,12 @@ module.exports = {
 
       env: {
         AMITABHA_MAIN_HOST: "https://namo-amitabha.herokuapp.com",
-        // DEBUG: "express-http-proxy",
       },
       // add development environment variables
       env_development: {
-        PORT: 3000,
-        SERVER_BASE_URL: "http://localhost:3000",
+        PORT: 8080,
+        SERVER_BASE_URL: "http://13.228.189.66",
+        DEBUG: "express-http-proxy",
       },
       // add production environment variables
       env_production: {
