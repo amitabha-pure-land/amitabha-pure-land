f77a384dd1ac726910ac389628a80055f986a076
Donghao Huang <donghao.huang@mastercard.com>
2021-09-21 07:09:10 +0800

    improve pm2 script

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -48,7 +48,7 @@ module.exports = {
         "npm install --only=prod && NODE_ENV=production npm run build && scp -r -i ~/.ssh/aws-amtf.pem .env* ubuntu@13.228.189.66:/home/ubuntu/code/amitabha-pure-land/deploy_buffer/ && scp -r -i ~/.ssh/aws-amtf.pem online/* ubuntu@13.228.189.66:/home/ubuntu/code/amitabha-pure-land/deploy_buffer/online/",
       // post-deploy action
       "post-deploy":
-        "npm install --only=prod && npm prune && rm -rf online && mv ../deploy_buffer/.env* . && mv ../deploy_buffer/online . && pm2 reload ecosystem.config.cjs --env production",
+        "npm install --only=prod && npm prune && rm -rf online && mv ../deploy_buffer/.env* . && mv ../deploy_buffer/online . && mkdir ../deploy_buffer/online && pm2 reload ecosystem.config.cjs --env production",
     },
   },
 };
