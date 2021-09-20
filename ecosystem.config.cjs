16bc6c03301df78622b15e327387c50d44aeaf6c
inflaton <inflaton.sg@gmail.com>
2021-09-20 18:28:01 +0800

    fix  ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -47,7 +47,7 @@ module.exports = {
         "npm install --only=prod && NODE_ENV=production npm run build && scp -r -i ~/.ssh/aws-amtf.pem online/* ubuntu@13.228.189.66:/home/ubuntu/code/amitabha-pure-land/current/online/",
       // post-deploy action
       "post-deploy":
-        "npm install --only=prod pm2 reload ecosystem.config.cjs --env production",
+        "npm install --only=prod && pm2 reload ecosystem.config.cjs --env production",
     },
   },
 };
