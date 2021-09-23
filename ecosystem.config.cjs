1b9bd89e9cfc20c99edecbf279f8ad55fabd33de
Donghao Huang <donghao.huang@mastercard.com>
2021-09-23 08:18:29 +0800

    prep github webhook

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,6 +8,9 @@ module.exports = {
       env: {
         PORT: 8080,
         SERVER_BASE_URL: "http://13.228.189.66",
+        GITHUB_URL:
+          "https://github.com/amitabha-pure-land/amitabha-pure-land.github.io.git",
+        GITHUB_SECRET: "5RiPxmZGKp_8f459d68b2a2",
       },
       // add development environment variables
       env_development: {
@@ -42,10 +45,11 @@ module.exports = {
       "pre-setup": "",
       // Post-setup commands or path to a script on the host machine
       // eg: placing configurations in the shared dir etc
-      "post-setup": "mkdir -p /home/ubuntu/code/amitabha-pure-land/deploy_buffer/",
+      "post-setup":
+        "mkdir -p /home/ubuntu/code/amitabha-pure-land/deploy_buffer/",
       // pre-deploy action
       "pre-deploy-local":
-        "npm install --only=prod && NODE_ENV=production npm run build && tar -czvf online.tar.gz online && scp -r -i ~/.ssh/aws-amtf.pem .env* ubuntu@13.228.189.66:/home/ubuntu/code/amitabha-pure-land/deploy_buffer/ && scp -r -i ~/.ssh/aws-amtf.pem online.tar.gz ubuntu@13.228.189.66:/home/ubuntu/code/amitabha-pure-land/deploy_buffer/ && rm online.tar.gz",
+        "npm install --only=prod && NODE_ENV=production npm run build-web && tar -czvf online.tar.gz online && scp -r -i ~/.ssh/aws-amtf.pem .env* ubuntu@13.228.189.66:/home/ubuntu/code/amitabha-pure-land/deploy_buffer/ && scp -r -i ~/.ssh/aws-amtf.pem online.tar.gz ubuntu@13.228.189.66:/home/ubuntu/code/amitabha-pure-land/deploy_buffer/ && rm online.tar.gz",
       // post-deploy action
       "post-deploy":
         "npm install --only=prod && npm prune && rm -rf online && mv ../deploy_buffer/.env* . && tar xvzf ../deploy_buffer/online.tar.gz && pm2 reload ecosystem.config.cjs --env production",
