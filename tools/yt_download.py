495688f8aea1025684fc8e9e2767bed8065af9d0
Donghao Huang <donghao.huang@mastercard.com>
2023-04-15 21:05:15 +0800

    improved youtube downloading scripts

--- tools/yt_download.py
+++ tools/yt_download.py
@@ -1,16 +1,16 @@
 import sys
-
-print(sys.argv)
 from pytube import YouTube
 
+print(sys.argv)
 youtube_video_url = sys.argv[1]
-folder = sys.argv[2] if len(sys.argv) > 2 else '.'
+filename = sys.argv[2] if len(sys.argv) > 2 else None
 
 retries = 5
 while retries > 0:
     try:
         youtube_video = YouTube(youtube_video_url)
-        filename = folder + '/' + youtube_video.title + '-720p.mp4'
+        if filename is None:
+            filename = youtube_video.title + '-720p.mp4'
         stream720p = youtube_video.streams.filter(type="video", res="720p")
         if len(stream720p) > 0:
             print("downloading: " + filename)
--- tools/yt_download.rb
+++ tools/yt_download.rb
@@ -8,17 +8,20 @@ def download_file(url, dest)
   nil
 end
 
-def process_file(module_html, yt_videos_path)
-    array = module_html.split('/')
-
+def process_file(module_html, yt_videos_path, start_index)
     doc = File.open(module_html) { |f| Nokogiri::HTML(f) }
 
+    i = 0
     doc.css('h1').each do |h1|
-      href = h1.parent.parent.last_element_child['href']
-      sub_module_title = h1.text
-      sub_module_title.strip!
-      puts "Processing #{sub_module_title} #{href} ..."
-      download_file(href, yt_videos_path)
+      i = i + 1
+      if i > start_index
+        href = h1.parent.parent.last_element_child['href']
+        sub_module_title = h1.text
+        sub_module_title.strip!
+        puts "Processing #{sub_module_title} #{href} ..."
+        dest = "'#{yt_videos_path}/#{sub_module_title}-720p.mp4'"
+        download_file(href, dest)
+      end
     end
  
   nil
@@ -26,16 +29,18 @@ end
 
 def main
     module_html = ARGV[0]
-    yt_videos_path = ARGV[1] || './yt_videos'
+    start_index = ARGV[1] || '0'
+    start_index = start_index.to_i
+    yt_videos_path = ARGV[2] || './yt_videos'
 
-    process_file(module_html, yt_videos_path)
+    process_file(module_html, yt_videos_path, start_index)
 end
 
 
 if __FILE__ == $0
   usage = <<-EOU
 
-usage: ruby #{File.basename($0)} module_html (optional) yt_videos_path app_id master_key server_url
+usage: ruby #{File.basename($0)} module_html (optional) start_index (optional) yt_videos_path
 
   EOU
 
